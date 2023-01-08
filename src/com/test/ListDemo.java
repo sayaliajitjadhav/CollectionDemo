ca62d2f144fed8ac24e0d05799a2919d1751971e
sayalijadhav <sayalijadhav85300@gmail.com>
2023-01-08 18:44:45 +0530

    added file for arraylist

---
+++ src/com/test/ListDemo.java
@@ -0,0 +1,15 @@
+package com.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListDemo {
+
+	public static void main(String[] args) {
+		List<String> list=new ArrayList<String>();
+		list.add("sayali");
+		list.add("ram");
+		list.add("nikita");
+		System.out.println("list>>>"+list);
+	}
+}
